c2913c70a79044ff18f8a572a8637de70bee1a99
TheAnswer07 <moisedia7@gmail.com>
2022-09-29 15:19:38 -0700

    Category routes set up

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,12 +3,29 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/categories', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll()
+  .then(categories => {
+    res.json(categories)
+  })
+  .catch(err => {
+    console.log(err);
+  })
 });
 
-router.get('/:id', (req, res) => {
+router.post('/categories', (req, res) => {
+  Category.create(req.body)
+  .then(category => {
+    res.json(category)
+  })
+  .catch(err => {
+    console.log(err);
+  })
+})
+
+router.get('/categories/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
 });
@@ -17,12 +34,26 @@ router.post('/', (req, res) => {
   // create a new category
 });
 
-router.put('/:id', (req, res) => {
+router.put('/categories/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {where: {id: req.params.id} })
+  .then(category => {
+    res.sendStatus(200)
+  })
+  .catch(err => {
+    console.log(err);
+  })
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/categories/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({where: {id: req.params.id} })
+  .then(category => {
+    res.sendStatus(200)
+  })
+  .catch(err => {
+    console.log(err);
+  })
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 const express = require('express');
-const routes = require('./routes');
+// const routes = require('./routes');
 const db = require('./config/connection')
 // import sequelize connection
+// const { Category, Product, ProductTag, Tag } = require('./models');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+// const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(routes);
+app.use(require('./routes'));
 
+
+db.sync();
+
+app.listen(process.env.PORT || 3001)
 // sync sequelize models to the database, then turn on the server
+// async function start() {
+//   await require('./config/connection').sync()
+//   app.listen(process.env.PORT || 3001)
+// }
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
+
+// start();
